90e6267d5cbe3341f5ebc32d60fbe32b1cafd3d9
cgraziano <cgraziano9363@gmail.com>
2017-03-29 22:56:15 -0600

    Section 1

---
+++ Introduction_S1L2.js
@@ -0,0 +1,5 @@
+     <!--
+     Learn by understanding and not reproducing.
+         -->
+       opensource community has developed frameworks (node.js, angular.js)
+    Javascript weird parts (understanding the how and whys)
